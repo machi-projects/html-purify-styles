dbefdeaed635b23053c66bcd5156ba6843510ce0
Inian Karthick <31567584+iniankarthick@users.noreply.github.com>
2017-09-02 20:04:46 +0530

    updated example-es6.js
    updated example-es6.js

--- test/example-es6.js
+++ test/example-es6.js
@@ -1,7 +1,7 @@
 import PurifyHtmlStyles from './purify-html-styles';
 
-let htmlString = `<div class='flet' style='color:red;display:none'> <span style='margin-left:10px'> testing </span> </div>`
-PurifyHtmlStyles purifyHtmlStyles = new PurifyHtmlStyles( htmlString );
+let htmlString = "<div class='flet' style='color:red;display:none'> <span style='margin-left:10px'> testing </span> </div>"
+let purifyHtmlStyles = new PurifyHtmlStyles( htmlString );
 htmlString = purifyHtmlStyles.run();
 
 console.log( htmlString );
