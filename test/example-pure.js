51cd96f54e10de686b4425620fe8636196db6a2f
Inian Karthick <31567584+iniankarthick@users.noreply.github.com>
2017-09-02 20:07:33 +0530

    example-pure-test .js
    example-pure-test .js

--- test/example-pure.js
+++ test/example-pure.js
@@ -1,7 +1,7 @@
-//Add the Purify Html Styles .js files
+//Add the Purify Html Styles .js files before run these
 
 var htmlString = "<div class='flet' style='color:red;display:none'> <span style='margin-left:10px'> testing </span> </div>"
-PurifyHtmlStyles purifyHtmlStyles = new PurifyHtmlStyles( htmlString );
+purifyHtmlStyles = new PurifyHtmlStyles( htmlString );
 htmlString = purifyHtmlStyles.run();
 
 console.log( htmlString );
